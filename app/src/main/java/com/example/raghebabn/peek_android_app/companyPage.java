f381b1390c06aa89e20d5c6f0b832daeb09cf61c
Connor Dear <connordear@gmail.com>
2018-09-22 20:30:53 -0400

    company page init
    Signed-off-by: Connor Dear <connordear@gmail.com>

--- app/src/main/java/com/example/raghebabn/peek_android_app/companyPage.java
+++ app/src/main/java/com/example/raghebabn/peek_android_app/companyPage.java
@@ -3,7 +3,7 @@ package com.example.raghebabn.peek_android_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class companyPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
